a9b951c70ac8d722bb5be86380257b1f8d8992bc
sadrawi <dr.sadrawi@gmail.com>
2025-10-06 14:18:45 +0700

    ok

--- app00.py
+++ app00.py
@@ -20,6 +20,8 @@ col1, col2, col3 = st.columns([1, 1, 1])
 with col2:
     st.image(image, use_container_width=True)
 
+
+st.markdown("<hr>", unsafe_allow_html=True)
 col1, col2, col3, col4, col5 = st.columns([1, 1, 5, 1, 1])
 with col3:
     st.title("AI-based Lung Nodule Segmentation")

9a689513479ef22499d0572a867ced2be37350e8
sadrawi <dr.sadrawi@gmail.com>
2025-10-06 12:48:00 +0700

    column width

--- app00.py
+++ app00.py
@@ -47,5 +47,5 @@ if uploaded_file:
         # Show result
         st.image(seg_img, 
             caption="Segmentation Result", 
-            use_column_width='always')
+            use_column_width=True)
 

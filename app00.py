40198148b4cc64d21051d78de9d4b65f85204af2
sadrawi <dr.sadrawi@gmail.com>
2025-10-06 14:24:32 +0700

    ok

--- app00.py
+++ app00.py
@@ -17,7 +17,8 @@ st.set_page_config(
 
 col1, col2, col3 = st.columns([1, 2, 1])
 with col2:
-    st.image("i3LUniversity.png", width=150)
+    st.image("i3LUniversity.png", 
+             use_container_width=True)
 
 
 st.markdown(
